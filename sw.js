9bbda889ab56ae078ead75a92745691f680bc362
Developer Evgeniy <79357491+Dev-Geniy@users.noreply.github.com>
2025-03-22 15:12:32 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,25 @@
+// sw.js
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('client-flow-v1').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/styles.css',
+        '/script.js',
+        '/favicon.png',
+        '/favicon-192x192.png',
+        '/favicon-512x512.png',
+        '/manifest.json'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
